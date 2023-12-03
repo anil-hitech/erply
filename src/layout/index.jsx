5cf684a15390d5b5958743eeeb46b4fd17c3e189
anil <anil@hitechvalley.com.au>
2023-12-03 14:12:37 +0545

    changed to dataSource to parmas

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -5,22 +5,22 @@ import { useSearchParams } from "react-router-dom";
 const Layout = () => {
   const [params] = useSearchParams();
 
-  // params.has("sessionKey") &&
-  //   localStorage?.setItem(
-  //     "clientDetail",
-  //     JSON.stringify({
-  //       clientCode: params?.get("clientCode"),
-  //       sessionKey: params?.get("sessionKey"),
-  //     })
-  //   );
+  params.has("sessionKey") &&
+    localStorage?.setItem(
+      "clientDetail",
+      JSON.stringify({
+        clientCode: params?.get("clientCode"),
+        sessionKey: params?.get("sessionKey"),
+      })
+    );
 
-  localStorage?.setItem(
-    "clientDetail",
-    JSON.stringify({
-      clientCode: "606950",
-      sessionKey: "038b36e0405fed269d59302f93308b204b0a2a8f77ed",
-    })
-  );
+  // localStorage?.setItem(
+  //   "clientDetail",
+  //   JSON.stringify({
+  //     clientCode: "606950",
+  //     sessionKey: "038b36e0405fed269d59302f93308b204b0a2a8f77ed",
+  //   })
+  // );
   return (
     <div className="flex flex-row ">
       <AppDrawer />
