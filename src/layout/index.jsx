0e142eb8548e50e5f132c6aef9a11fe8fb550847
anil <anil@hitechvalley.com.au>
2023-12-03 08:15:11 +0545

    fix:client detail session storage

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -4,15 +4,32 @@ import { useSearchParams } from "react-router-dom";
 
 const Layout = () => {
   const [params] = useSearchParams();
-  localStorage?.setItem(
-    "clientDetail",
-    JSON.stringify({
-      clientCode: params?.get("clientCode"),
-      sessionKey: params?.get("sessionKey"),
-      // clientCode: "606950",
-      // sessionKey: "fcbeac150450adfe23c4b6c5ac5fe1f727b087e60cb2",
-    })
-  );
+  const clientDetail =
+    JSON?.parse(localStorage?.getItem("clientDetail")) || null;
+
+  //sets localstorage if there
+  if (clientDetail) {
+    params?.get("sessionKey") !== clientDetail.sessionKey &&
+      localStorage?.setItem(
+        "clientDetail",
+        JSON.stringify({
+          clientCode: params?.get("clientCode"),
+          sessionKey: params?.get("sessionKey"),
+          // clientCode: "606950",
+          // sessionKey: "ad364a169cdfacb7de7ecb3fbe8fee1a7555692433f0",
+        })
+      );
+  } else {
+    localStorage?.setItem(
+      "clientDetail",
+      JSON.stringify({
+        clientCode: params?.get("clientCode"),
+        sessionKey: params?.get("sessionKey"),
+        // clientCode: "606950",
+        // sessionKey: "ad364a169cdfacb7de7ecb3fbe8fee1a7555692433f0",
+      })
+    );
+  }
 
   return (
     <div className="flex flex-row ">
