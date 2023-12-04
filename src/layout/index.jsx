bf6696074d71a04d50e9051480b86c948bf0557d
anil <anil@hitechvalley.com.au>
2023-12-04 10:58:24 +0545

    session key changed

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -5,22 +5,22 @@ import { useSearchParams } from "react-router-dom";
 const Layout = () => {
   const [params] = useSearchParams();
 
-  // params.has("sessionKey") &&
-  //   localStorage?.setItem(
-  //     "clientDetail",
-  //     JSON.stringify({
-  //       clientCode: params?.get("clientCode"),
-  //       sessionKey: params?.get("sessionKey"),
-  //     })
-  //   );
+  params.has("sessionKey") &&
+    localStorage?.setItem(
+      "clientDetail",
+      JSON.stringify({
+        clientCode: params?.get("clientCode"),
+        sessionKey: params?.get("sessionKey"),
+      })
+    );
 
-  localStorage?.setItem(
-    "clientDetail",
-    JSON.stringify({
-      clientCode: "606950",
-      sessionKey: "6eef69dfe27c40dc26ca09d803d61d04f0cdbccbb45c",
-    })
-  );
+  // localStorage?.setItem(
+  //   "clientDetail",
+  //   JSON.stringify({
+  //     clientCode: "606950",
+  //     sessionKey: "6eef69dfe27c40dc26ca09d803d61d04f0cdbccbb45c",
+  //   })
+  // );
   return (
     <div className="flex flex-row ">
       <AppDrawer />
