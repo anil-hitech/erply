99ee2ed83b41bd81e7363a9ffbe501e2d2df205c
anil <anil@hitechvalley.com.au>
2024-01-18 12:43:39 +0545

    feat: dynamic hostname check for session storage

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -4,22 +4,26 @@ import { useSearchParams } from "react-router-dom";
 const Layout = () => {
   const [params] = useSearchParams();
 
-  // params.has("sessionKey") &&
-  //   localStorage?.setItem(
-  //     "clientDetail",
-  //     JSON.stringify({
-  //       clientCode: params?.get("clientCode"),
-  //       sessionKey: params?.get("sessionKey"),
-  //     })
-  //   );
+  console.log(window.location.hostname);
+  if (window.location.hostname == "localhost") {
+    localStorage?.setItem(
+      "clientDetail",
+      JSON.stringify({
+        clientCode: "606950",
+        sessionKey: "4cfd8bd906848e0a52dc3f3eb9cd5c8c7a10ec57d28f",
+      })
+    );
+  } else {
+    params.has("sessionKey") &&
+      localStorage?.setItem(
+        "clientDetail",
+        JSON.stringify({
+          clientCode: params?.get("clientCode"),
+          sessionKey: params?.get("sessionKey"),
+        })
+      );
+  }
 
-  localStorage?.setItem(
-    "clientDetail",
-    JSON.stringify({
-      clientCode: "606950",
-      sessionKey: "4cfd8bd906848e0a52dc3f3eb9cd5c8c7a10ec57d28f",
-    })
-  );
   return (
     <div className="flex flex-row ">
       <AppDrawer />
